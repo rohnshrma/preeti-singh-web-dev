254689d489db89e81fbe62fac9f606ed17c1ef8a
rohnshrma <rohn11shrma@gmail.com>
2024-06-06 09:22:00 +0530

    first commit

---
+++ javascript/functions.js
@@ -0,0 +1,65 @@
+// function greet(){
+//     console.log("hello world");
+// }
+
+// greet()
+// greet()
+// greet()
+// greet()
+// greet()
+// greet()
+
+
+// function greet(name){
+//     console.log("hello world my name is" , name);
+// }
+
+// greet()
+// greet("preeti")
+// greet("rohan")
+// greet("preeti singh")
+
+
+// function calcBmi(weight,height,name){
+//     var bmi = weight / height ** 2
+//     if (bmi < 18.5){
+//         console.log(`${name}! you're underweight as your bmi is ${bmi}`);
+//     }else if (bmi >= 18.5 && bmi < 25){
+//         console.log(`${name}! you're of normal weight as your bmi is ${bmi}`);
+//     }else if (bmi >= 25 && bmi <30){
+//         console.log(`${name}! you're of overweight as your bmi is ${bmi}`);
+//     }else{
+//         console.log(`${name}! you're obesse as your bmi is ${bmi}`);
+//     }
+// }
+
+// calcBmi(80,1.80,"preeti")
+// calcBmi(100,1.80,"John")
+
+// create a function which takes two numbers as parameters and logs the sum of it
+
+
+
+
+// function addition(x,y){
+//     console.log(x+y);
+// }
+// addition(12,23)
+
+
+// create a function which takes an array as a parameter and log all items of it using for loop or forEach
+
+
+// function outputItems(arr){
+    //     // for(var i = 0 ; i < arr.length ; i++){
+        //     //     console.log(arr[i]);
+        //     // }
+        
+        //     arr.forEach(function(item){
+            //         console.log(item);
+            //     })
+            // }
+            
+            // outputItems([2,34,23,34,45,56,5,67])
+            
+// create a function which takes an array as a parameter and log the sum of all items in it.(using for loop or forEach) 
